cd3f4deb6a203e4274c892ce84d49b6052da2f35
Thercístenes <thercistenes1@outlook.com>
2024-02-23 17:08:32 -0300

    adiciona arquivo products no diretório model

---
+++ backend/src/models/products.model.js
@@ -0,0 +1,29 @@
+const camelize = require('camelize');
+const connection = require('./connection');
+
+const findAll = async () => {
+  const [products] = await connection.execute('SELECT * FROM products');
+  return camelize(products);
+};
+
+const findById = async (productId) => {
+  const [[product]] = await connection.execute(
+    'SELECT * FROM products WHERE id = ?',
+    [productId],
+  );
+  return camelize(product);
+};
+
+const create = async (name) => {
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO products (name) VALUES (?)',
+    [name],
+  );
+  return { id: insertId, name };
+};
+
+const update = async (id, name) => {
+  await connection.execute('UPDATE products SET name = ? WHERE id = ?', [name, id]);
+  return { id, name };
+};
+module.exports = { findAll, findById, create, update };
