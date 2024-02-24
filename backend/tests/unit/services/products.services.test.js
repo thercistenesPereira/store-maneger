b5298bea8b7aa715e833044aa4c8715a98bb30ac
Thercístenes <thercistenes1@outlook.com>
2024-02-24 18:25:43 -0300

    adiciona teste na função update services

--- backend/tests/unit/services/products.services.test.js
+++ backend/tests/unit/services/products.services.test.js
@@ -1,12 +1,42 @@
-// const chai = require('chai');
-// const sinon = require('sinon');
+const chai = require('chai');
+const sinon = require('sinon');
 // const connection = require('../../../src/models/connection');
-// const productServices = require('../../../src/services/products.service');
+const productServices = require('../../../src/services/products.service');
+const productsModel = require('../../../src/models/products.model');
 
-// const { expect } = chai;
+const { expect } = chai;
 
-describe('productsServices', function () {
-  it('Ao chamar o método update em GET /product/:id com um id de produto inexistente, deve retornar um objeto com status NOT_FOUND e uma mensagem de erro', async function () {
-    //
+describe('productsService', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  it('Ao chamar o método update na rota GET /product/:id com um id de produto inexistente, deve retornar um objeto com status NOT_FOUND e uma mensagem de erro', async function () {
+  // Arranjo
+    const id = 1;
+    sinon.stub(productsModel, 'findById').resolves(null);
+
+    // Ação
+    const result = await productServices.update(id);
+
+    // Assertiva
+    expect(result).to.deep.equal({ 
+      status: 'NOT_FOUND', 
+      data: { message: 'Product not found' }, 
+    });
+  });
+
+  it('Ao chamar o método update na rota GET /product/:id com um id de produto válido, deve retornar um array', async function () {
+    // Arranjo
+    const products = [
+      { id: 1, name: 'Martelo de Thor' },
+      { id: 2, name: 'Traje de encolhimento' },
+    ];
+    sinon.stub(productsModel, 'findById').resolves(products);
+  
+    // Ação
+    const result = await productServices.update();
+  
+    // Assertiva
+    expect(result).to.deep.equal(products);
   });
 });
